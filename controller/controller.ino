a1be90311b37d655a5baa26d6fb2467724e3d163
Zachary Stewart <zstewar1@umbc.edu>
2013-12-06 21:25:57 -0500

    added control pins

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,18 @@
+// number of pins to control a motor.
+#define pincount 4
+// number of motors available
+#define numMotors 2
+
+// the pins for controlling each motor
+const byte motorpins[][] = {{2,3,4,5},{6,7,8,9}};
+
+// current step positions of each motor.
+byte currentpos[] = {0, 0};
+
 void setup(){
+  
 }
 
 void loop(){
+  
 }
