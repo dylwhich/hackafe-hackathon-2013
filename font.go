ccab7371323ee8ff774a19f8f5be8a34694bffc6
Dylan Whichard <dylan@whichard.com>
2013-12-07 08:40:50 -0500

    Add font type for loading json font

---
+++ font.go
@@ -0,0 +1,92 @@
+package text
+
+import (
+	"encoding/json"
+	"hackathon/ncscreen"
+	"os"
+)
+
+type Glyph struct {
+	Lines     []Line
+	Character rune
+	Joining   bool
+}
+
+type Font map[rune]Glyph
+
+func (font *Font) GetGlyph(char rune) Glyph {
+	result, ok := font[rune]
+	if !ok {
+		result = font[rune('_')]
+	}
+
+	return result
+}
+
+func LoadFont(fileName string) *Font {
+	file, err := os.open(fileName)
+	if err != nil {
+		panic("Error opening " + fileName + ": " + err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	v := make(map[string]interface{}, 0)
+	err := decoder.Decode(v)
+	if err != nil {
+		panic("Unable to decode json: " + err)
+	}
+
+	font := make(Font, len(v))
+
+	for key, val := range v {
+		obj, ok := val.(map[string][]float64)
+		if !ok {
+			panic("Can't cast this font")
+		}
+
+		if len(obj.x) != len(obj.y) {
+			panic("Glyph " + key + " has incompatible lengths")
+		}
+
+		lines = make([]Line, 0)
+		lineNum := 0
+
+		lastX := -1
+		lastY := -1
+		for i := 0; i < len(obj.x); i++ {
+			x := obj["x"][i]
+			y := obj["y"][i]
+
+			if x != -1 && y != -1 {
+				if lastX != -1 && lastY != -1 {
+					lines[lineNum] = Line{
+						Start: Coord{
+							X: lastX,
+							Y: lastY,
+						},
+						End: Coord{
+							X: x,
+							Y: y,
+						},
+					}
+
+					lineNum++
+				}
+
+				lastX = x
+				lastY = y
+			}
+
+		}
+
+		font[rune(key[0])] = Glyph{
+			Lines:     lines,
+			Character: rune(key[0]),
+			Joining:   false,
+		}
+	}
+
+	return &font
+}
