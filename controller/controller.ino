80b6db9b21b07fcd1d064271d70e49e9815699b0
Zachary Stewart <zstewar1@umbc.edu>
2013-12-07 00:59:48 -0500

    added interlace stuff

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,10 @@
 #define numPins 4
 #define numMotors 2
+#define stepDelay 10
+#define sign(X) (X < 0 ? -1 : X > 0 1 : 0)
+
+#define turnMotor 't'
+#define interlace 'i'
 
 const byte motor1pins[] = {2,3,4,5};
 const byte motor2pins[] = {6,7,8,9};
@@ -8,6 +13,8 @@ const byte* motorpins[] = {motor1pins, motor2pins};
 byte motorstep[] = {0, 0};
 
 void setup(){
+  Serial.begin(9600);
+  
   for(byte motor = 0; motor < numMotors; motor++){
     for(byte pin = 0; pin < numPins; pin++){
       pinMode(motorpins[motor][pin], OUTPUT);
@@ -19,8 +26,61 @@ void setup(){
 }
 
 void loop(){
-  stepmotor(1,200);
-  stepmotor(1,-200);
+  if(Serial.available()){
+     char marker = Serial.read();
+     
+     if(marker == turnMotor){
+       // wait for 3 bytes: 1 for a motor number, 2 for a number of turns
+       while(Serial.available() < 3);
+       
+       byte motorNum = Serial.read();
+       
+       byte nums[2];
+       Serial.readBytes(nums, 2);
+       
+       int numSteps = *nums;
+       
+       stepmotor(motorNum, numSteps);
+     }
+     if(marker == interlace){
+       while(Serial.available() < 4);
+       
+       byte nums[4];
+       Serial.readBytes(nums, 4);
+       
+       int motor1dist = *nums;
+       int motor2dist = *(nums + 2);
+       
+       byte motor1dir = sign(motor1dist);
+       byte motor2dir = sign(motor2dist);
+       
+       motor1dist = abs(motor1dist);
+       motor2dist = abs(motor2dist);
+       
+       if(motor1dist > motor2dist){         
+         onepertwo = motor1dist/motor2dist;
+         
+         while(motor1dist > 0 || motor2dist > 0){
+           stepmotor(0, min(motor1dist, onepertwo) * motor1dir;
+           stepmotor(1, min(1, motor2dist) * motor2dir;
+           
+           motor1dist = max(motor1dist - onepertwo, 0);
+           motor2dist = max(motor2dist - 1, 0);
+         }
+       }
+       else{
+         twoperone = motor2dist/motor1dist;
+         
+         while(motor1dist > 0 || motor2dist > 0){
+           stepmotor(1, min(motor2dist, twoperone) * motor2dir;
+           stepmotor(0, min(1, motor1dist) * motor1dir;
+           
+           motor2dist = max(motor2dist - twoperone, 0);
+           motor1dist = max(motor1dist - 1, 0);
+         }
+       }
+     }
+  }
 }
 
 void stepmotor(byte motornum, int numSteps){
@@ -38,25 +98,6 @@ void stepmotor(byte motornum, int numSteps){
       digitalWrite(motorpins[motornum][motorstep[motornum]], HIGH);
     }
     
-    delay(5);
+    delay(stepDelay);
   }
 }
-
-//void stepmotor1(int numSteps){
-//  for(int i = 0; i < abs(numSteps); i++){
-//    if(numSteps > 0){
-//      digitalWrite(motor1pins[motor1step], LOW);
-//      motor1step += 1;
-//      if(motor1step >= numPins) motor1step = 0;
-//      digitalWrite(motor1pins[motor1step], HIGH);
-//    }
-//    else{
-//      digitalWrite(motor1pins[motor1step], LOW);
-//      motor1step -= 1;
-//      if(motor1step >= numPins) motor1step = numPins - 1;
-//      digitalWrite(motor1pins[motor1step], HIGH);
-//    }
-//    
-//    delay(5);
-//  }
-//}
