37103880f602a2c28d4e0fddb4a8e5ccbabd2287
Zachary Stewart <zstewar1@umbc.edu>
2013-12-06 21:50:24 -0500

    Added setup

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,18 +1,32 @@
 // number of pins to control a motor.
-#define pincount 4
+#define numPins 4
 // number of motors available
 #define numMotors 2
+
+#define degPerStep 1.8
+#define distPerDeg 1
 
 // the pins for controlling each motor
 const byte motorpins[][] = {{2,3,4,5},{6,7,8,9}};
 
 // current step positions of each motor.
-byte currentpos[] = {0, 0};
+byte currentstep[] = {0, 0};
 
 void setup(){
-  
+  for(byte i = 0; i < numMotors; i++){
+    for(byte k = 0; i < numPins; k++){
+      pinMode(motorpins[i][k], OUTPUT);
+      
+      if(currentstep[i] == k){
+        digitalWrite(motorpins[i][k], HIGH);
+      }
+      else { 
+        digitalWrite(motorpins[i][k], LOW);
+      }
+    }
+  }
 }
 
 void loop(){
-  
+ 
 }
